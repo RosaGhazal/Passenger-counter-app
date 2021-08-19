5f5de5df9796e7ea2fd4c2a4574609fe37086311
Rosa Ghazal <49504070+RosaGhazal@users.noreply.github.com>
2021-08-19 13:04:43 -0700

    Add files via upload

---
+++ Passenger counter app/index.js
@@ -0,0 +1,23 @@
+
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+increment();
+
+
+function save() {
+    let saveCount = count + " - " ;
+    saveEl.textContent += saveCount;
+    count = 0;
+    countEl.textContent = 0;
+}
+
+save();
+
